17f9d66f6dfdd590c4a6206321f2b21a8be9b38c
Feiwuguanfang <110074865+Feiwuguanfang@users.noreply.github.com>
2022-10-18 23:04:20 +0800

    Add files via upload

---
+++ 折半查找/折半查找.cpp
@@ -0,0 +1,42 @@
+﻿#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n, key, num[100], low, high, mid;
+    scanf_s("%d", &n);
+    getchar();
+    for (int i = 0; i < n; i++)
+    {
+        scanf_s("%d,", &num[i]);
+    }
+    getchar();
+    scanf_s("%d", &key);
+    high = n - 1;
+    low = 0;
+    mid = (high + low) / 2;
+    while (low < high)
+    {
+        if (key == num[mid])
+        {
+            printf("%d", mid + 1);
+            break;
+        }
+        if (key < num[mid])
+        {
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+        if (key > num[mid])
+        {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        }
+        if (low == high)
+        {
+            printf("%d", high + 1);
+        }
+    }
+    return 0;
+
+
+}
